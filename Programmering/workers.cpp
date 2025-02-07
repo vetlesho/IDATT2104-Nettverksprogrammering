e6d92cb509f47cf6f7933794cd692c8c00aaecf0
vetlesho <vetlesho@stud.ntnu.no>
2025-02-07 12:04:19 +0100

    Finish task 2, worker threads

--- Programmering/workers.cpp
+++ Programmering/workers.cpp
@@ -7,15 +7,20 @@
 #include <functional>
 #include <chrono>
 
+using namespace std;
+
 // to compile
 // g++ -std=c++17 -o workers_program workers.cpp -pthread
 // to run
 // ./workers_program
 
+
 class Workers {
     public:
-        explicit Workers(size_t numThreads) : stopFlag(false){
-            this->numThreads = numThreads;
+        explicit Workers(size_t numThreads) : stopFlag(false) {
+            for (size_t i = 0; i < numThreads; ++i) {
+                threads.emplace_back([this] { workerLoop(); });
+            }
         }
 
         void start() {
@@ -25,47 +30,58 @@ class Workers {
             }
         }
 
-        void post(std::function<void()> task) {
+        void post(function<void()> task) {
             {
-                std::lock_guard<std::mutex> lock(queueMutex);
-                taskQueue.push(std::move(task));
+                lock_guard<mutex> lock(queueMutex);
+                taskQueue.push(task);
             }
             cv.notify_one();
+        }
+    
+        // adds task to the queue with a delay
+        void post_timeout(function<void()> task, int seconds) {
+            {
+                lock_guard<mutex> lock(queueMutex);
+                taskQueue.push([seconds, task] {
+                    this_thread::sleep_for(chrono::seconds(seconds));
+                    task();
+                });
+            }
+            cv.notify_one(); // notifies next worker thread 
         }
 
         void stop() {
             {
-                std::lock_guard<std::mutex> lock(queueMutex);
+                lock_guard<mutex> lock(queueMutex);
                 stopFlag = true;
             }
             cv.notify_all(); // notify remainig threads to end
         }
 
         void join() {
+            stop();
             for (auto &thread : threads) {
                 if (thread.joinable()) {
                     thread.join();
                 }
             }
         }
-
-
-
+        
 
     private:
         size_t numThreads;
-        std::vector<std::thread> threads; // Dynamic threads, needs multiple worker threads
-        std::queue<std::function<void()>> taskQueue; // taskqueue for storing functions a task in a worker thread will do
-        std::mutex queueMutex; // ensure one thread accesses the taskqueue at a time
-        std::condition_variable cv; // condition variable, sleep until notified
+        vector<thread> threads; // Dynamic threads, needs multiple worker threads
+        queue<function<void()>> taskQueue; // taskqueue for storing functions a task in a worker thread will do
+        mutex queueMutex; // ensure one thread accesses the taskqueue at a time
+        condition_variable cv; // condition variable, sleep until notified
         bool stopFlag;
 
         void workerLoop() {
             while (true) {
-                std::function<void()> task;
+                function<void()> task;
                 {
                     // Each thread safely chooses a task
-                    std::unique_lock<std::mutex> lock(queueMutex);
+                    unique_lock<mutex> lock(queueMutex);
                     cv.wait(lock, [this] { return stopFlag || !taskQueue.empty(); });
 
                     if (stopFlag && taskQueue.empty()) return;
@@ -85,28 +101,30 @@ int main() {
     worker_threads.start(); // create 4 internal thread
     event_loop.start();     // create 1 internal thread
 
-    worker_threads.post([] {
-        std::cout << "Task A\n"; 
-    });
-    worker_threads.post([] {
-        std::cout << "Task B\n";
-    });
+    worker_threads.post_timeout([] {
+        cout << "Task A\n"; 
+    }, 3);
+    worker_threads.post_timeout([] {
+        cout << "Task B\n";
+    }, 1);
 
-    event_loop.post([] {
-        std::cout << "Task C\n";
-    });
-    event_loop.post([] {
-        std::cout << "Task D\n";
-    });
+    event_loop.post_timeout([] {
+        cout << "Task C\n";
+    }, 1);
+    event_loop.post_timeout([] {
+        cout << "Task D\n";
+    }, 1);
 
-    worker_threads.stop();
+    //this_thread::sleep_for(chrono::seconds(3));
+
+    worker_threads.join(); // Calls join() on the worker threads
+    event_loop.join();     // Calls join() on the event thread
+
+    /*worker_threads.stop();
     worker_threads.join();
 
     event_loop.stop();
-    event_loop.join();
-
-    //worker_threads.join(); // Calls join() on the worker threads
-    //event_loop.join();     // Calls join() on the event thread
+    event_loop.join();*/
 
     return 0;
 }
